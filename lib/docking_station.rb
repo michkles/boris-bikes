c39dfccfc4901c64fc7e220264b9d4aea89c0d5c
michkles <michkles@gmail.com>
2016-09-29 21:52:03 +0100

    step 17 rspec tests fixes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,45 +3,37 @@
 class DockingStation
 
 attr_accessor :capacity
+attr_reader :bike
+
 DEFAULT_CAPACITY = 20
 
   def initialize(capacity = DEFAULT_CAPACITY)
-    @bike_array = []
+    @bike_array = Array.new
     @capacity = capacity
-
   end
 
-
-
-  #attr_reader :bike #Use method #bike to check bike docked at station
-  #
-  def bike
-    @bike_array #shows all bikes at the station
-  end
 
   def release_bike
     fail 'No bikes available' unless !empty?
-    @bike_array.pop
+    bike_array.pop
   end
 
    def dock(bike)
      # We need to return the bike we dock
      fail 'Docking station full' if full?
-     @bike_array.push(bike)
+     bike_array.push(bike)
   end
 
 private
 
+attr_reader :bike_array
+
   def full?
-    @bike_array.count >= capacity
+    bike_array.count >= capacity
   end
 
   def empty?
-    @bike_array.empty?
+    bike_array.empty?
   end
 
 end
-
-
-#manchester = DockingStation.new
-#suthportred = Bike.new
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,70 +4,46 @@ describe DockingStation do
 
 
   it 'has default capacity' do
-      expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-    end
-
-
-    describe 'initialization' do
-<<<<<<< HEAD
-      subject { DockingStation.new }
-      let(:bike) { Bike.new }
-      it 'defaults capacity' do
-        DockingStation::DEFAULT_CAPACITY.times do
-          subject.dock(bike)
-        end
-        expect{ subject.dock(bike) }.to raise_error 'Docking station full'
-      end
-    end
-
-=======
-  it 'has a variable capacity' do
-    docking_station = DockingStation.new(50)
-    50.times { docking_station.dock Bike.new }
-    p docking_station
-    expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
-end
->>>>>>> 29975f91b1e215cc83b6a1b9404f39673bd8dfa5
 
+  describe 'initialization' do
+    it 'has a variable capacity' do
+      docking_station = DockingStation.new(50)
+      50.times { docking_station.dock Bike.new }
+      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
 
-=begin
-  it 'returns docked bikes' do
+ it 'returns docked bikes' do
+   bike = Bike.new
+   subject.dock(bike)
+   expect(subject.release_bike).to eq bike
+ end
+
+ it { is_expected.to respond_to :release_bike }
+
+ it 'releases working bikes' do
+   subject.dock(Bike.new)
+   bike = subject.release_bike #FIX HERE
+   expect(bike).to be_working
+ end
+
+ it { is_expected.to respond_to (:bike) }
+
+ describe '#release_bike' do
+   it 'returns error when no bike available' do
+     expect {subject.release_bike}.to raise_error 'No bikes available'
+   end
+ end
+
+ describe '#dock' do
+   it 'returns error when docking station is full' do
     bike = Bike.new
-    subject.dock(bike)
-expect(subject.bike.pop).to eq bike
-  end
-=end
-
-  it { is_expected.to respond_to :release_bike }
-
-  it 'releases working bikes' do
-    subject.dock(Bike.new)
-    bike = subject.release_bike #FIX HERE
-    expect(bike).to be_working
-  end
-
-<<<<<<< HEAD
-  #it { is_expected.to respond_to :bike }
-=======
-  it { is_expected.to respond_to (:bike) }
->>>>>>> 29975f91b1e215cc83b6a1b9404f39673bd8dfa5
-
-  describe '#release_bike' do
-    it 'returns error when no bike available' do
-      #subject.release_bike
-      expect {subject.release_bike}.to raise_error 'No bikes available'
-    end
-  end
-
-  describe '#dock' do
-    it 'returns error when docking station is full' do
-    #subject.dock(Bike.new)
-      bike = Bike.new
-    DockingStation::DEFAULT_CAPACITY.times do
-     subject.dock(bike)
-      #expect {subject.dock(bike)}.to raise_error 'Docking station at capacity'
-    end
-  end
+    DockingStation::DEFAULT_CAPACITY.times {subject.dock(bike)}
+    expect {subject.dock(bike)}.to raise_error 'Docking station full'
+   end
  end
- end
+
+
+end
