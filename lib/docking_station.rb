afdd1c3f00d187baa5b9eb5c972e2754e3d07d6f
michkles <michkles@gmail.com>
2016-09-29 11:29:11 +0100

    step15 added private methods: full?, empty?

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,6 +5,7 @@ class DockingStation
   def initialize
     @bike_array = Array.new
 
+
   end
 
   #attr_reader :bike #Use method #bike to check bike docked at station
@@ -14,13 +15,28 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' unless !@bike_array.empty?
+    fail 'No bikes available' unless !empty?
     @bike_array.pop
   end
 
    def dock(bike)
      # We need to return the bike we dock
-     fail 'Docking station at capacity' if @bike_array.length >= 20
+     fail 'Docking station at capacity' if full?
      @bike_array.push(bike)
   end
+
+private
+
+  def full?
+    @bike_array.count >= 20
+  end
+
+  def empty?
+    @bike_array.empty?
+  end
+
 end
+
+
+#manchester = DockingStation.new
+#suthportred = Bike.new
