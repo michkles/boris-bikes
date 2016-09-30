f5c4f778fb2896199569358354ea5d1d4d9afe1d
michkles <michkles@gmail.com>
2016-09-30 09:24:39 +0100

    added unit test for report_broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,13 @@ describe Bike do
   it { is_expected.to respond_to :working?}
 
   it { is_expected.to respond_to :report_broken }
+
+  describe "#report_broken" do
+    it "change working bike to false" do
+      bike = Bike.new
+      bike.report_broken
+      expect(bike.working?).to be_falsey
+    end
+  end
+
 end
